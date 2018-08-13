687694ea79eaa86174c73c2ea9fa043a979eeeb1
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-13 18:13:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 var prefix = ("--")
 
-bot.on('ready', function() {
-	bot.user.setActivity(`jsui heberger Cool`);
+client.on('ready', function() {
+	client.user.setActivity(`jsui heberger Cool`);
 	console.log("Conections en cour")
 });
 
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
 
-bot.on('message', message => {
+client.on('message', message => {
 
    if (message.content === prefix + "ping"){
    message.channel.send("est non je tes troll xD")
