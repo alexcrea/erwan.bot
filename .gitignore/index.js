e0fe0ddabbf90dc73cc89b046e0c017d1a3f7984
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-13 19:23:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,6 @@ const { Client, MessageAttachment } = require('discord.js');
 const fs = require('fs');
 const { version } = require("discord.js");
 const moment = require("moment");
-require("moment-duration-format");
 
 
 var prefix = ("--")
@@ -20,6 +19,46 @@ client.on('message', message => {
 
 var msg = message 
 
+if(command === `${prefix}fight`) {
+    //checks if the username to fight is in the message
+    let author1 = message.author.username;
+    let user = message.mentions.users.first();
+    if(!user) return message.reply("you did not specify who you would like to fight!");
+
+    //checks if the users is trying to fight themselves
+    if(user.id == message.author.id) return message.reply('you cannot fight yourself!');
+
+    //checks if the user is trying to fight the bot
+    if(user.bot ==  true)
+        return message.reply('you cannot fight a bot!');
+
+    //saves the two user ids to variables
+    var fighter1 = message.author.id;
+    var fighter2 = user.id;
+
+    //announces challenge and awaits response
+    var challenged = user.toString();
+    message.channel.send(`${challenged}, ${author1} has challenged you to a duel. Do you accept the challenge, yes or no?`)
+        .then(() => {
+            message.channel.awaitMessages(response => response.content == 'yes' && response.author.id == fighter2 || response.content == 'no' && response.author.id == fighter2, {
+                max: 1,
+                time: 60000,
+                errors: ['time'],
+            })
+            .then((collected) => {
+                if (collected.first().content == 'yes') {
+                    message.channel.send(`${challenged} has accepted the challenge!`);
+                }
+                else if(collected.first().content == 'no') {
+                    message.channel.send(`nope`);
+                }
+            })
+            .catch(() => {
+                message.channel.send(`No response. Fight has been cancelled.`);
+            });
+        });       
+}
+
  if (message.content === '--ping') {
   message.channel.send(`**${message.author.username} Pong! \nLa latence est** ${msg.createdTimestamp - message.createdTimestamp}ms`)
    
@@ -27,7 +66,7 @@ var msg = message
 
  
  
-  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+ 
    if(message.content == '--botinfo') {
 
 	
@@ -59,10 +98,6 @@ var msg = message
                     name: ':timer:Onligne Timer',
                     value: `${duration}`,
           },
-         {
-                    name: ':computer:Discord.js Versions',
-                    value: `v${version}`,
-           },
           {
                     name: ':computer:Node.js Versions',
                     value: `${process.version}`,
