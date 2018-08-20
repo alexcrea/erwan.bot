fa83cacce18b4e92f65f94973cf76bd04dafa3ce
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-20 22:42:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -315,9 +315,8 @@ message.channel.send({embed: {
  
 });
 
-//client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
- // console.log(`Une Nouvelle guilde a ajouter mon bot : ${guild.name} (id: ${guild.id}). Cette guilde a ${guild.memberCount} Membre!`);
-// guild.channels.get('479011053494337538').send('**Une Nouvelle guilde a ajouter mon bot : ${guild.name} (id: ${guild.id}). Cette guilde a ${guild.memberCount} Membre!**');
-//});
+client.on("guildCreate", guild => {
+  console.log(`Une Nouvelle guilde a ajouter mon bot : ${guild.name} (id: ${guild.id}). Cette guilde a ${guild.memberCount} Membre!`);
+   client.channels.get('479011053494337538').send(`Une Nouvelle guilde a ajouter mon bot : ${guild.name} (id: ${guild.id}). Cette guilde a ${guild.memberCount} Membre!`);
+});
 
